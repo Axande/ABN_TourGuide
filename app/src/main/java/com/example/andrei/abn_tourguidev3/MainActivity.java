3910a3260868cf7afffbf0266b771eaeeb900870
Andrei <Axande@users.noreply.github.com>
2018-03-12 18:14:20 +0100

    Feedback implementation

--- app/src/main/java/com/example/andrei/abn_tourguidev3/MainActivity.java
+++ app/src/main/java/com/example/andrei/abn_tourguidev3/MainActivity.java
@@ -55,44 +55,101 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addLocations() {
-        allLocations.add(new Location("Vapiano",
-                "Vapiano is a European restaurant franchise company headquartered in Bonn." +
-                        " The chain's restaurants offer Italian food according to the fast-casual principle." +
-                        " In April 2017, Vapiano totaled 180 locations in 31 countries, including Australia, " +
-                        "China, Saudi Arabia and the United States. For many years, the company was among the " +
-                        "fastest-growing players in the industry.",
-                "51.4379756,5.477862,17z?q=vapiano",
-                "040 760 2070", 2, R.drawable.vapiano, "https://nl.vapiano.com/nl/home/"));
+        allLocations.add(new Location(getString(R.string.vapiano_title),
+                getString(R.string.vapiano_description),
+                getString(R.string.vapiano_location),
+                getString(R.string.vapiano_phone),
+                2,
+                R.drawable.vapiano,
+                getString(R.string.vapiano_website)));
 
-        allLocations.add(new Location("Café 't Lempke", "Everyone in Eindhoven knows 't Lempke. " +
-                "From what? From the hours of dancing for, next to and especially at the bar. " +
-                "Every Thursday, Friday and Saturday, 't Lempke is full of nightlife, what makes 't Lempke one of the " +
-                "most crowded nightlife locations in Eindhoven. But people also know 't Lempke as the permanent home " +
-                "of many student associations . ",
-                "51.4367399,5.478519,17zq=Café 't Lempke", "06 52655204", 3, R.drawable.lempke, "http://lempke.nl/"));
+        allLocations.add(new Location(getString(R.string.lempke_title),
+                getString(R.string.lempke_description),
+                getString(R.string.lempke_location),
+                getString(R.string.lempke_phone),
+                3,
+                R.drawable.lempke,
+                getString(R.string.lempke_website)));
 
-        allLocations.add(new Location("Bar Bistro Calypso", "Bar Bistro Calypso openend on January first this year." +
-                " It is located on Stratumseind, right in the middle of the street.",
-                "51.4359714,5.4807166,17zq=Bar Bistro Calypso", "040 368 0691", 3, R.drawable.calypso, "http://bistrocalypso.nl"));
+        allLocations.add(new Location(getString(R.string.calypso_title),
+                getString(R.string.calypso_description),
+                getString(R.string.calypso_location),
+                getString(R.string.calypso_phone),
+                3,
+                R.drawable.calypso,
+                getString(R.string.calypso_website)));
 
-        allLocations.add(new Location("Evoluon", "The Evoluon is a conference centre and former science museum erected" +
-                " by the electronics and electrical company Philips at Eindhoven in the Netherlands in 1966." +
-                " Since its construction, it has become a landmark and a symbol for the city.",
-                "51.443633,5.4410833,17zq=Evoluon", "0", 1, R.drawable.evoluon));
+        allLocations.add(new Location(getString(R.string.evoluon_title),
+                getString(R.string.evoluon_description),
+                getString(R.string.evoluon_location),
+                getString(R.string.evoluon_phone),
+                1,
+                R.drawable.evoluon));
 
+
+        allLocations.add(new Location(getString(R.string.philips_title),
+                getString(R.string.philips_description),
+                getString(R.string.philips_location),
+                getString(R.string.philips_phone),
+                0,
+                R.drawable.philips));
+
+        allLocations.add(new Location(getString(R.string.gp_title),
+                getString(R.string.gp_description),
+                getString(R.string.gp_location),
+                getString(R.string.gp_phone),
+                1,
+                R.drawable.gp));
+
+        allLocations.add(new Location(getString(R.string.effenaar_title),
+                getString(R.string.effenaar_description),
+                getString(R.string.effenaar_location),
+                getString(R.string.effenaar_phone),
+                3,
+                R.drawable.effenar));
+
+        allLocations.add(new Location(getString(R.string.abbemuseum_title),
+                getString(R.string.abbemuseum_description),
+                getString(R.string.abbemuseum_location),
+                getString(R.string.abbemuseum_phone),
+                0,
+                R.drawable.abbemuseum,
+                getString(R.string.abbemuseum_website)));
+
+
+        allLocations.add(new Location(getString(R.string.psv_title),
+                getString(R.string.psv_description),
+                getString(R.string.psv_location),
+                getString(R.string.psv_phone),
+                0,
+                R.drawable.psv,
+                getString(R.string.psv_website)));
+
+        allLocations.add(new Location(getString(R.string.daf_title),
+                        getString(R.string.daf_description),
+                        getString(R.string.daf_location),
+                        getString(R.string.daf_phone),
+                        0,
+                        R.drawable.daf,
+                    getString(R.string.daf_website)));
+
+        //Code for generating dummy data sets
         for (int i = 1; i <= 1000; i++) {
             if (randomBool()) //to add dummy image or not
-                allLocations.add(new Location("Dummy data " + i, "Lorem Ipsum is simply dummy text of the printing and " +
-                        "typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when " +
-                        "an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived " +
-                        "not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-                        "51.4455368,5.3814704,12zq=Eindhoven", "0", getRandomInt(5)));
+                allLocations.add(new Location(
+                        getString(R.string.dummy_title) + i,
+                        getString(R.string.dummy_description),
+                        getString(R.string.dummy_location),
+                        getString(R.string.dummy_phone),
+                        getRandomInt(5)));
             else
-                allLocations.add(new Location("Dummy data " + i, "Lorem Ipsum is simply dummy text of the printing and " +
-                        "typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when " +
-                        "an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived " +
-                        "not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-                        "0", "0", getRandomInt(5), R.drawable.dummy_img));
+                allLocations.add(new Location(
+                        getString(R.string.dummy_title) + i,
+                        getString(R.string.dummy_description),
+                        getString(R.string.dummy_empty_location) ,
+                        getString(R.string.dummy_phone),
+                        getRandomInt(5),
+                        R.drawable.dummy_img));
         }
     }
 
